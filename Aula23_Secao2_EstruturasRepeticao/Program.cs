ae775a8a1e010779a4c960f6a361a9d12c102938
AdamsEric <eric.adsbrito@live.com>
2018-05-21 21:13:54 -0300

    Aula 23

---
+++ Aula23_Secao2_EstruturasRepeticao/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Aula23_Secao2_EstruturasRepeticao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 20; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
